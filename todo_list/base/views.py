dda8fb5f628765f0559e4eb97e30fa59e9f41338
deepakatAviSoft <deepak.sharma@avisoft.io>
2024-03-12 13:13:58 +0530

    version1.3
    > created a new html file

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks' #See the task_list.html 
 
+
+class TaskDetail(DetailView):
+    model = Task 
+    context_object_name = 'task' #by using this we can change {{object}} name to {{task}} in html file
 
 
 
